5c126236e9cb5ed58b40a170809ef6a0dff17e87
Zcy2000 <80466178+Zcy2000@users.noreply.github.com>
2023-07-09 16:05:48 +0800

    Update order.sol

--- contracts/order.sol
+++ contracts/order.sol
@@ -1,26 +1,25 @@
-// contracts/GameItem.sol
-// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ArtistNFT is ERC721URIStorage, ERC721Enumerable {
+contract Order is ERC721URIStorage, ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
  
-    constructor() ERC721("ArtistNFT", "AN") {
+    constructor() ERC721("Order", "AN") {
        
     }
 
-    function mint(address artist, string memory tokenURI)
+    function mint(address order, string memory tokenURI)
         public
         returns (uint256)
     {
   
         uint256 newItemId = _tokenIds.current();
-        _mint(artist, newItemId);
+        _mint(order, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
         _tokenIds.increment();
